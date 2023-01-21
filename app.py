a3c994d91fb4141bd6fb99359cc8d546e15dd497
tessyXavier <101969047+tessyXavier@users.noreply.github.com>
2023-01-21 12:56:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.LUX]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app.title = 'Analysis on OTT platforms in India'
 server = app.server
 app.config.suppress_callback_exceptions = True
